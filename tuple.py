e3da133dc9b2aa3779afe70ad07e85602ac09a7b
myra-2512 <abhi.back2back@gmail.com>
2025-09-13 11:02:26 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,15 @@
+tuplex=("Tuple",False,20)
+print(tuplex)
+
+tuplex1=(23,34,45,56,67,78,89,90)
+print(tuplex1)
+
+print(tuplex + tuplex1)
+
+print(tuplex1.count(56))
+
+print(tuplex1[4:6])
+
+print(tuplex1[:3])
+
+print(len(tuplex))
